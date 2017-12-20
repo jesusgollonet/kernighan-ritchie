a021f43e2ae0ce8132299f0097f12f3459402872
Jesus Gollonet <jesusgollonet@gmail.com>
2017-12-20 12:49:27 +0100

    Modify 1.6 to count specific instances of digits

--- 0106-countdigitsetal.c
+++ 0106-countdigitsetal.c
@@ -9,23 +9,31 @@ int isWhitespace(c){
     return c  == ' ' || c == '\t' || c == '\n';
 }
 
-int isPlainChar(c){
-    return !(isWhitespace(c) || isDigit(c));
-}
-
 int main(){
     int c;
     int digitCount, whitespaceCount, plainCharCount;
+    int digits[10];
+
     digitCount = whitespaceCount = plainCharCount = 0;
+
+    for (int i = 0; i < 10; i++){
+        digits[i] = 0;
+    }
 
     while((c = getchar())!= EOF){
         if(isDigit(c)) {
-            ++digitCount;
+            ++digits[ c - '0'] ;
         } else if (isWhitespace(c)){
             ++whitespaceCount; 
         } else {
             ++plainCharCount; 
         }
     }
-    printf("hey, that sentence has %i digits, %i whitespace chars, and %i normal chars", digitCount, whitespaceCount, plainCharCount);
+
+    printf("hey, that sentence has:\n");
+
+    for (int i = 0; i < 10; i++){
+        printf("digit %i: %i\n", i, digits[i]);
+    }
+    printf("%i whitespace chars, and\n%i normal chars", whitespaceCount, plainCharCount);
 }
